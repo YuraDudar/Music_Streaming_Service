a7878fd539418c455fdc47a48343862312819944
MeDGranD <ivanred289@gmail.com>
2025-05-31 21:32:15 +0300

    structure project load

---
+++ stream-service/src/test/java/ru/SberPo666/stream_service/StreamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.SberPo666.stream_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
