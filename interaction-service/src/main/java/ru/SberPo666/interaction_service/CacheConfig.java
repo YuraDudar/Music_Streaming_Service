eece303df16051d14a8f93f13a6c5d057acac589
MeDGranD <ivanred289@gmail.com>
2025-06-01 17:35:50 +0300

    feat: adde caching

---
+++ interaction-service/src/main/java/ru/SberPo666/interaction_service/CacheConfig.java
@@ -0,0 +1,39 @@
+package ru.SberPo666.interaction_service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType(Object.class)
+                .build();
+        objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
+
+        GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jsonRedisSerializer))
+                .disableCachingNullValues();
+    }
+}
--- interaction-service/src/main/java/ru/SberPo666/interaction_service/repository/PlaylistRepository.java
+++ interaction-service/src/main/java/ru/SberPo666/interaction_service/repository/PlaylistRepository.java
@@ -1,5 +1,9 @@
 package ru.SberPo666.interaction_service.repository;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.SberPo666.interaction_service.Entity.PlaylistEntity;
@@ -14,6 +18,7 @@ public interface PlaylistRepository extends JpaRepository<PlaylistEntity, UUID> 
             Select * from playlists where user_id = :userId
             """,
     nativeQuery = true)
+    @Cacheable(value = "playlistsByUserId", key = "#userId")
     List<PlaylistEntity> getPlaylistByUserId(UUID userId);
 
     @Query(value =
@@ -21,6 +26,7 @@ public interface PlaylistRepository extends JpaRepository<PlaylistEntity, UUID> 
             Select * from playlists where playlist_id = :playlistId
             """,
             nativeQuery = true)
+    @Cacheable(value = "playlistById", key = "#playlistId")
     PlaylistEntity getPlaylistById(UUID playlistId);
 
     @Query(value =
@@ -28,6 +34,23 @@ public interface PlaylistRepository extends JpaRepository<PlaylistEntity, UUID> 
             Select position from playlist_tracks where playlist_id = :playlistId order by position desc
             """,
             nativeQuery = true)
+    @Cacheable(value = "playlistLastPosition", key = "#playlistId")
     Integer getLastPositionInPlaylistById(UUID playlistId);
+
+    @Override
+    @Caching(
+            put = { @CachePut(value = "playlistById", key = "#result.id") },
+            evict = {
+                    @CacheEvict(value = "playlistsByUserId", key = "#result.userId")
+            }
+    )
+    <S extends PlaylistEntity> S save(S entity);
+
+    @Override
+    @Caching(evict = {
+            @CacheEvict(value = "playlistById", key = "#id"),
+            @CacheEvict(value = "playlistLastPosition", key = "#id")
+    })
+    void deleteById(UUID id);
 
 }
